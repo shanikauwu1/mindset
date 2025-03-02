c02941f94170a9ff654646538b9246cb9ba572ef
shanika Jayawardane <shanikauwu@gmail.com>
2025-03-02 15:07:52 -0700

    add the day 4 exercises and task 1,2,3

---
+++ assets/js/color-scroll-top.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollTop = document.getElementById("scroll-top");
+
+  if (scrollTop) {
+    scrollTop.style.color = "red"; // Change the color as needed
+  }
+});
